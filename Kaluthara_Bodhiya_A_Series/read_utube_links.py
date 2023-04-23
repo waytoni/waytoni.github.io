3a96898e01383e9648b41a7a8612dd159ffcb116
waytoni <122663193+waytoni@users.noreply.github.com>
2023-04-24 01:10:49 +1000

    Updated file names

--- Kaluthara_Bodhiya_A_Series/read_utube_links.py
+++ Kaluthara_Bodhiya_A_Series/read_utube_links.py
@@ -48,7 +48,7 @@ with open(text_filename, 'w', encoding="utf-8") as fp:
     fp.write('<a href="../Paramartha_Video/Paramartha_Video.html">පරමාර්ථ ලෝකය දේශනා </a>\n \
         <a href="../Anichcha_Dukka_Anathma_Series/Anichcha_Dukka_Anathma.html">අනිච්ච, දුක්ඛ, අනත්ත දේශනා </a>\n \
         <a href="../Zoom_Info/zoom_info.html">Join Zoom Live Class </a>\n \
-        <a href="../Chithatha_Chithisika/Chiththa_Chithisika.html">චිත්ත සහ චෛතිසික </a>\n')
+        <a href="../Chithatha_Chithasika/Chiththa_Chithasika.html">චිත්ත සහ චෛතසික</a>\n')
     fp.write('<a href="javascript:void(0);" class="icon" onclick="navFunction()"> <i class="fa fa-bars"></i></a>\n')
     fp.write('</div>\n')
     
