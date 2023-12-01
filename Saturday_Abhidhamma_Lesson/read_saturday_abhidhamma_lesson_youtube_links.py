b0494468137c2f820fd006d48a93ca98dc096c46
waytoni <122663193+waytoni@users.noreply.github.com>
2023-12-01 23:49:42 +1100

    Added Nivan Maga Udesa 16 and Abhidhamma Aruth B11

---
+++ Saturday_Abhidhamma_Lesson/read_saturday_abhidhamma_lesson_youtube_links.py
@@ -0,0 +1,47 @@
+import sys
+
+# print the original sys.path
+print('Original sys.path:', sys.path)
+
+# append a new directory to sys.path
+sys.path.append('scripts')
+
+# print the updated sys.path
+# print('Updated sys.path:', sys.path)
+
+from helpers import ReadSections
+from helpers import *
+
+
+
+notes_file = 'Saturday_Abhidhamma_Lesson/saturday_abhidhamma_lesson_notes.txt'  
+utube_links = 'Saturday_Abhidhamma_Lesson/saturday_abhidhamma_lesson_youtube_links.txt'
+html_file = 'Saturday_Abhidhamma_Lesson/index.html'
+playlist_url = 'https://www.youtube.com/playlist?list=PLqESXbJ82aIip-TA7Efg5JjwmEDJ95kAx'
+
+series_title = 'තුන්කල්හි වෙනස් නොවන ලොව එකම විශ්ව දර්ශනය - දේශනා'
+
+sections = ReadSections(notes_file)
+
+# print(sections)
+
+print(html_file)
+PrepareHead(html_file, series_title)
+
+with open(html_file, 'a', encoding='utf-8') as fp:
+    fp.write('<h1>තුන්කල්හි වෙනස් නොවන ලොව එකම විශ්ව දර්ශනය - දේශනා</h1>\n')
+
+    fp.write('<h2>තුන්කල්හි වෙනස් නොවන ලොව එකම විශ්ව දර්ශනය..... </h2>\n')
+    fp.write('<p>මානවයාගේ වැරදි දෘෂ්ඨීන්ගෙන් මුදවා ගැනීමේ අදහසින්, තරුණ දූදරුවන්ට ගැලපෙන පරිදි, Science විෂයට (සම්මුති විද්‍යා විෂයට) ගලපමින් පවත්වන දේශනා මාලාව. සරලව මුල සිට……. </p>\n')
+    
+    fp.write('<p></p>\n')
+    fp.write('සජීවි දේශනා සෑම සෙනසුරාදා සවස 2.00 සිට (ලංකාවේ වේලාවෙන්) <a href="../Zoom_Info/zoom_info.html">Zoom සැසියට සම්බන්ද වීමට</a>)\n')
+    fp.write('<p></p>\n')
+
+    # fp.write('<p><a href="../Nivan_Maga_Udesa/zoom_details.html">Zoom සජීවීව සම්බන්ධ වීමට</a></p>\n')
+    fp.close()
+
+block_id = ''
+idx_prefix = 'A'
+
+HtmlDropdownBlock(block_id, utube_links, series_title, html_file, playlist_url, idx_prefix, sections)
--- build_it.py
+++ build_it.py
@@ -12,11 +12,12 @@
 # <index> <youtube link (URL)> <date>
 
 from Kaluthara_Bodhiya_A_Series import Read_Sat2pm_youtube_links
+from Saturday_Abhidhamma_Lesson import read_saturday_abhidhamma_lesson_youtube_links
 # from All_Playlists import waytoni_all_videos
 
 from All_Playlists import read_Iseries
 from All_Playlists import read_utube_links_all
-from Nivan_Maga_Udesa import read_nivan_maga_udesa_youtube_links_C
+from Nivan_Maga_Udesa import read_nivan_maga_udesa_youtube_links
 from Suthamaya import read_suthamaya_youtube_links
 from Abhidharma_Aruth import read_Abhidharma_Aruth_youtube_links
 import sys
@@ -25,10 +26,11 @@ sys.path.append('E:/src/github/waytoni_io/waytoni_desktop/scripts_new')
 import waytoni_all_videos_new
 
 Read_Sat2pm_youtube_links
+read_saturday_abhidhamma_lesson_youtube_links
 read_Iseries
 # waytoni_all_videos
 waytoni_all_videos_new
 read_utube_links_all
-read_nivan_maga_udesa_youtube_links_C
+read_nivan_maga_udesa_youtube_links
 read_suthamaya_youtube_links
 read_Abhidharma_Aruth_youtube_links
