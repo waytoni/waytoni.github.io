423db897826a37a4d42434c67485759eeb117b1d
waytoni <122663193+waytoni@users.noreply.github.com>
2024-03-22 00:08:55 +1100

    Added Abhidharma Aruth Desana on 24/03/21

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -249,6 +249,11 @@ def PrepareHead(text_filename, series_title):
             tag_info = ftag.read()
             fp.write(tag_info)
             ftag.close()
+            
+        fp.write('<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">\n')
+        fp.write('<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">\n')
+        fp.write('<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">\n')
+        fp.write('<link rel="manifest" href="/site.webmanifest">\n')    
         
         fp.write('\n')    
         fp.write('\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n')
