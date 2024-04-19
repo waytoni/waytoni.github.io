966f031358efd8d513687ab75d0b8c757eb5fbe0
waytoni <122663193+waytoni@users.noreply.github.com>
2024-04-20 00:25:31 +1000

    Added Nivan Maga Udesa 35

---
+++ scripts/py/build_series_Suthamaya.py
@@ -0,0 +1,49 @@
+import sys
+
+# print the original sys.path
+# print('Original sys.path:', sys.path)
+
+import os
+sys.path.append('scripts/py')
+
+# print the updated sys.path
+# print('Updated sys.path:', sys.path)
+
+from utilities import *
+
+
+
+basepath = 'Suthamaya'
+
+intro_file = os.path.join(basepath,'suthamaya_intro.html')
+notes_file = os.path.join(basepath,'suthamaya_eththapana_notes.txt')
+utube_links = os.path.join(basepath,'suthamaya_eththapana.txt')
+html_file = os.path.join(basepath,'index.html')
+
+playlist_url = ''
+
+series_title = 'සුතමයඤාණං - ඉත්තෑපාන අක්කර දේශනා'
+
+print(intro_file)
+print(notes_file)
+print(utube_links)
+print(html_file)
+
+sections = ReadSections(notes_file)
+
+PrepareHead(html_file, series_title)
+
+
+with open(html_file, 'a', encoding='utf-8') as fp:
+    
+    with open(intro_file, 'r', encoding='utf-8') as fintro:
+        page_intro = fintro.read()
+        fp.write(page_intro)
+        fintro.close()
+
+block_id = ''
+idx_prefix = ''
+
+HtmlDropdownBlock(block_id, utube_links, series_title, html_file, playlist_url, idx_prefix, sections)
+
+PrepareTail_2ndLevel(html_file)
