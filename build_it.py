7a9195edc4feaf8e66400d89193cdbb3c9332ff7
waytoni <168102732+upulgun@users.noreply.github.com>
2024-06-21 22:49:13 +1000

    Added Nivan Maga Udesa 42

--- build_it.py
+++ build_it.py
@@ -8,18 +8,10 @@
 # then add an entry to the last line of the file:
 #   KalutharaBhodiya/J_series//J_series.txt
 #
-# Both files have the following format
+# All files have the following format
 # <index> <youtube link (URL)> <date>
 
-# from Kaluthara_Bodhiya_A_Series import Read_Sat2pm_youtube_links
-# from Saturday_Abhidhamma_Lesson import read_saturday_abhidhamma_lesson_youtube_links
-# from All_Playlists import waytoni_all_videos
 
-# from All_Playlists import read_Iseries
-# from All_Playlists import read_utube_links_all
-# from Nivan_Maga_Udesa import read_nivan_maga_udesa_youtube_links
-# from Suthamaya import read_suthamaya_youtube_links
-# from Abhidharma_Aruth import read_Abhidharma_Aruth_youtube_links
 
 from scripts.py import build_series_Nivan_Maga_Udesa
 from scripts.py import build_series_Saturday_Abhidhamma_Lesson
