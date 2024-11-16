72f71586cc72e40a56a3e5fc0fa5a1303087802d
waytoni <168102732+upulgun@users.noreply.github.com>
2024-11-16 22:14:40 +1100

    Updated all videos

--- build_web.py
+++ build_web.py
@@ -11,28 +11,18 @@
 # YouTube links should be saved in the following format
 # <index> <youtube link (URL)> <date>
 
-# from Kaluthara_Bodhiya_A_Series import Read_Sat2pm_youtube_links
-# from Saturday_Abhidhamma_Lesson import read_saturday_abhidhamma_lesson_youtube_links
-# from All_Playlists import read_Iseries
-# from All_Playlists import read_utube_links_all
-# from Nivan_Maga_Udesa import read_nivan_maga_udesa_youtube_links
-# from Suthamaya import read_suthamaya_youtube_links
-# from Abhidharma_Aruth import read_Abhidharma_Aruth_youtube_links
 
-from scripts.py import build_series_Nivan_Maga_Udesa
-from scripts.py import build_series_Saturday_Abhidhamma_Lesson
+# from scripts.py import build_series_Nivan_Maga_Udesa
+from scripts.py import build_series_NivanMagaUdesa
+# from scripts.py import build_series_Saturday_Abhidhamma_Lesson
+from scripts.py import build_series_AbhidharmaASeries
+from scripts.py import build_series_AbhidharmaMulaSita
 from scripts.py import build_series_Abhidharma_Aruth
-from scripts.py import build_series_Suthamaya
+from scripts.py import build_series_AbhidharmaAruth
+from scripts.py import build_series_SuthamayaHirigal
 from scripts.py import build_series_SiyaluDesana
-from scripts.py import build_series_J_old
-
-# Read_Sat2pm_youtube_links
-# read_saturday_abhidhamma_lesson_youtube_links
-# read_Iseries
-# read_utube_links_all
-# read_nivan_maga_udesa_youtube_links
-# read_suthamaya_youtube_links
-# read_Abhidharma_Aruth_youtube_links
+#from scripts.py import build_series_J_old
+from scripts.py import build_series_J
 
 
 menu_change = False
@@ -50,14 +40,24 @@ if menu_change == True:
     from scripts.py import build_file_list
     # when new images are added run create_thubnails.py in Documents
     build_file_list
-    
+    from scripts.py import build_page_ChiththaChithasika
+    build_page_ChiththaChithasika
     # Update
-    # Chithatha_Chithasika, and Vishesha Desana
+    # Chithatha_Chithasika (old page), and Vishesha Desana
     # also update all_videos header
+    # KalutharaBodhiya B C D Batches html
+    # Zoom info Nivan Maga Udesa, and AbhidhmmaASeries
+   
 
-build_series_Nivan_Maga_Udesa
-build_series_Saturday_Abhidhamma_Lesson
+
+# build_series_Nivan_Maga_Udesa
+build_series_NivanMagaUdesa
+# build_series_Saturday_Abhidhamma_Lesson
+build_series_AbhidharmaASeries
+build_series_AbhidharmaMulaSita
 build_series_Abhidharma_Aruth
-build_series_J_old
-build_series_Suthamaya
+build_series_AbhidharmaAruth
+#build_series_J_old
+build_series_J
+build_series_SuthamayaHirigal
 build_series_SiyaluDesana
