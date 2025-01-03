a2166be4ab89089725c98409f996a88e5b350057
waytoni <168102732+upulgun@users.noreply.github.com>
2025-01-04 01:20:09 +1100

    Updated Abhidharma Aruth desana

--- scripts/py/build_series_AbhidharmaAruth.py
+++ scripts/py/build_series_AbhidharmaAruth.py
@@ -28,6 +28,41 @@ playlist_url = ''
 
 series_title = 'පොල්ගස්ඕවිට පැවෙත්වෙන නිවන් මග උදෙසා දර්ශන ඥාණය දේශනා මාලාව'
 
+AA_series_styles = """
+   	<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 10px;
+		}
+
+		p {
+			margin: 0px;
+		}
+
+		#video-container {
+			margin-top: 20px;
+		}
+
+		#notes {
+			margin-top: 20px;
+			white-space: pre-wrap;
+		}
+
+		#controls {
+			margin-top: 20px;
+		}
+  
+        #sutta {
+			margin-left: 20px;
+			margin-right: 20px;
+        }
+		#sutta p {
+			text-align: left;
+		}
+
+	</style>
+"""
+
 print(intro_file)
 print(notes_file)
 print(utube_links)
@@ -37,7 +72,8 @@ print(json_file)
 # build the json file from the youtube links and notes files
 BuildDropDownMenuWithNavigation(utube_links, notes_file, json_file)
 
-PrepareHead(html_file, series_title)
+# PrepareHead(html_file, series_title)
+PrepareHeadTop(html_file, series_title, AA_series_styles)
 
 with open(html_file, 'a', encoding='utf-8') as fp:
     
