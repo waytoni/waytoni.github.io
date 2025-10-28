f00f92416487528d1d35022c700a4d77c5cbca43
waytoni <168102732+upulgun@users.noreply.github.com>
2025-10-29 09:51:02 +1100

    Updated website

--- scripts/py/tempCodeRunnerFile.py
+++ scripts/py/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-    for pdf_file in pdf_dir.glob('*.pdf'):
-        print(pdf_file)
-        if search_pdf_for_terms(pdf_file, search_terms):
-            matching_files.append(pdf_file.name)
+BuildDropDownMenuWithNavigation
