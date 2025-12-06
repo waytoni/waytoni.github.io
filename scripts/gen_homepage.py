3bcead50a368491e61e4049125b7c0865baa7487
waytoni <168102732+upulgun@users.noreply.github.com>
2025-12-06 23:48:22 +1100

    Updated homepage update scripts

--- scripts/gen_homepage.py
+++ scripts/gen_homepage.py
@@ -181,8 +181,8 @@ def generate_homepage():
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     info_file = os.path.join(base_dir, 'Homepage', 'homepage_info.txt')
     template_file = os.path.join('scripts', 'templates/homepage_template.html')
-    navigation_file = os.path.join(os.path.dirname(__file__), 'navigation_header.html')
-    navigation_file = 'scripts/py/navigation_header.html'  # Adjusted path
+    navigation_file = os.path.join(os.path.dirname(__file__), 'navigation_header_template.html')
+    navigation_file = 'scripts/templates/navigation_header.html'  # Adjusted path
     messages_file = os.path.join(base_dir, 'Homepage', 'messages_new.html')
     output_file = os.path.join(base_dir, 'index.html')
     
